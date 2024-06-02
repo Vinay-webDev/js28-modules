dce71b06861c19c72728c9860219fc56fba7bcab
ninjaprogrammer2999 <149578035+ninjaprogrammer2999@users.noreply.github.com>
2024-06-02 14:08:36 +0530

    1. some points to remember while working with modules

---
+++ js/main.js
@@ -0,0 +1,26 @@
+// modules
+// 1. ***you need to have local server running to make modules
+// 2. need to have type="module" in script tag (and remeber when we use type="module" there's no need use defer or put script at the bottom it will automatically do that for us)
+// 3. *** when we use type="module" it immediately initiate the "use strict"; 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
