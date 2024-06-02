0210cd6d6d9c76dd12cab619b8a475754c7ad7af
ninjaprogrammer2999 <149578035+ninjaprogrammer2999@users.noreply.github.com>
2024-06-02 14:48:22 +0530

    3. another method to export

--- js/playing.js
+++ js/playing.js
@@ -1,4 +1,4 @@
-
+/*
 const fighting = () => {
     return "playing fighting game!";
 }
@@ -13,9 +13,29 @@ const horror = () => {
 // here we need to export one as default 
 export default fighting;
 // we can also export remaining functions as well
-export { shooting, horror};
+export { shooting, horror}; */
+////////////////////////////////////////
+// another method to export 
+// *** remember while exporting default function we must need to use function keyword **** 
+export default function fighting () {
+    return "playing fighting game!";
+}
+///or 
+export const shooting = () => {
+    return "playing shooting game!";
+}
 
+export const horror = () => {
+    return "playing horror game!";
+}
+////////////////////////////////////
+/*export function shooting () {
+    return "playing shooting game!";
+}
 
+export function horror () {
+    return "playing horror game!";
+}*/
 
 
 
